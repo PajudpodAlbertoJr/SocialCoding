84c09fe641f9909c9f274c5161f4f1bedd3f45ee
PajudpodAlbertoJr <61365883+PajudpodAlbertoJr@users.noreply.github.com>
2022-10-27 06:38:46 +0000

    Update ipcountpopulation.py

--- ipcountpopulation.py
+++ ipcountpopulation.py
@@ -1,6 +1,7 @@
 import requests
 from requests import get
 
-loc = (get('https://ipapi.co/json/').json())
-loc = loc['country_population']
-print (f"Your IP country population is: {loc}")
+def population():
+  loc = (get('https://ipapi.co/json/').json())
+  loc = loc['country_population']
+  print (f"Your IP country population is: {loc}")
