134d271639b0b01cd2e33ea7bd4fa58c6b82c0ad
PajudpodAlbertoJr <albertojrpajudpod@gmail.com>
2022-10-27 03:47:28 +0000

    updated ipapi

--- IPAPI.py
+++ IPAPI.py
@@ -2,8 +2,6 @@ from urllib import response
 import requests
 from requests import get
 
-from requests import get
-
 loc = (get('https://ipapi.co/json/').json())
 loc = loc['ip']
 print (f"Your public IP is: {loc}")
