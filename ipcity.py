2e564e149b3cf0d7213850e15af20f566cdf7c5f
PajudpodAlbertoJr <61365883+PajudpodAlbertoJr@users.noreply.github.com>
2022-10-27 06:55:54 +0000

    Update ipcity.py

--- ipcity.py
+++ ipcity.py
@@ -1,6 +1,9 @@
 import requests
 from requests import get
 
-loc = (get('https://ipapi.co/json/').json())
-loc = loc['city']
-print (f"Your IP city location is: {loc}")
+#Gets the IP address city location from ipapi.co
+
+def ip_city():
+  loc = (get('https://ipapi.co/json/').json())
+  loc = loc['city']
+  print (f"Your IP city location is: {loc}")
