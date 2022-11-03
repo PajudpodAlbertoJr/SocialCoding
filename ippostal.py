81ee3b49fb1b9b1a13dc83f907e67baa07d9f637
CyraDato <qclpdato@tip.edu.ph>
2022-11-03 04:28:35 +0000

    updated

--- ippostal.py
+++ ippostal.py
@@ -1,9 +1,17 @@
+import requests 
 from requests import get 
 
-#gets the IP address postal code from ipapi.co
+#import the request library
 
-def postal():
-  loc = (get('https://ipapi.co/json/').json())
-  loc = loc['postal']
 
-  print (f"Your IP city postal is: {loc}")
+def ip_postal():
+    loc = (get('https://ipapi.co/json/').json())
+    loc = loc['postal']
+    print (f"Your IP city postal is: {loc}")
+
+def in_ip_postal(ip):
+    loc = get(f'https://ipapi.co/{ip}/json/')
+    loc = loc.json(['postal'])
+    print(f'The postal location of IP, {ip} is: {loc}')
+
+
