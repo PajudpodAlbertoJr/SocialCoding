5021f3e4728a4d374f96d2bb8c5e6415f6a86c50
Raymond Co <90268548+rjco-tip@users.noreply.github.com>
2022-11-03 12:03:13 +0800

    Update ipcountpopulation.py

--- ipcountpopulation.py
+++ ipcountpopulation.py
@@ -7,3 +7,8 @@ def population():
   loc = (get('https://ipapi.co/json/').json())
   loc = loc['country_population']
   print (f"Your IP country population is: {loc}")
+  
+def ip_population(ip):
+  loc = get(f'https://ipapi.co/{ip}/json/')
+  loc = loc.json()['country_population']
+  print(f"The population count of {ip} is: {loc}")
